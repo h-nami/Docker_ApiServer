d29480d5281f3628c5ce9171d2d1ae04e1ad0491
HonamiOta <h.oota0705@gmail.com>
2024-09-05 11:38:02 +0900

    Create ArticlesTableSeeder

---
+++ database/seeders/ArticlesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('articles')->insert([
+            [
+                'title' => 'タイトル1',
+                'body' => '内容1'
+            ],
+            [
+                'title' => 'タイトル2',
+                'body' => '内容2'
+            ],
+            [
+                'title' => 'タイトル3',
+                'body' => '内容3'
+            ],
+        ]);
+    }
+}
